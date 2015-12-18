c795f4ec4545f771484dee5625d841bf01751274
Abigail Hahn <Abigail.Hahn@systemsbiology.org>
2015-12-18 14:16:02 -0800

    Updated the indexing examples script

--- grid-engine/samtools-index/index_bam_files.py
+++ grid-engine/samtools-index/index_bam_files.py
@@ -3,8 +3,8 @@ import os
 import json
 import tempfile
 import argparse
-import cgc_gauth
-import cgc_curl
+import isb_auth
+import isb_curl
 import subprocess
 from shutil import copyfile
 from urllib import urlencode
@@ -18,46 +18,62 @@ api_name = 'cohort_api'
 api_ver = 'v1';
 bam_pattern = '^.*\.bam$'
 
-def index_bam_files(file_list, storage, job_name, output_bucket, logs_bucket, grid_computing_tools_dir, copy_original_bam=False):
+def index_bam_files(file_list, storage, job_name, output_bucket, logs_bucket, grid_computing_tools_dir, copy_original_bams, dry_run):
+	print copy_original_bams, dry_run
+	exit(0)
 	# Create a text file containing the file list (one file per line)
-	text_file_list = tempfile.mkstemp()
-	config_file = tempfile.mkstemp()
+	text_file_list = tempfile.NamedTemporaryFile(delete=False)
+	config_file = tempfile.NamedTemporaryFile(delete=False)
 	
-	with open(text_file_list[0], 'w') as f:
-		exit(0)
+	with open(text_file_list.name, 'w') as f:
 		for isb_cgc_bam_file in file_list:
-			if copy_original_bam:
+			if copy_original_bams:
 				# Update the path to the new location in the user's cloud storage space
-				my_bam_file = bam_file.split('.')[-1]
+				my_bam_file = isb_cgc_bam_file.split('.')[-1]
 				new_path = os.path.join(output_bucket, my_bam_file)
 
+				# Get the source bucket name and object
+				source = isb_cgc_bam_file.split('/')
+				source_bucket = source[2]
+				source_object = '/'.join(source[3:])
+
+				# Get the destination bucket name
+				destination = new_path.split('/')
+				destination_bucket = destination[2]
+
 				# Copy the file to be indexed to the output bucket
-				# Question 1: how do we get the bucket name for the files?
-				# Question 2: which bucket contains the Percivall Pott TCGA cohort BAM files currently?
-				try:
-					response = storage.copy(sourceBucket="", sourceObject=isb_cgc_bam_file, destinationBucket=output_bucket, destinationObject=my_bam_file, body={}).execute()
-				except: 
-					# not sure what this will throw yet
-					pass
+				if dry_run:
+					print "would have copied source bucket %s, source object %s, to destination bucket %s, destination object %s"
+				else:
+					try:
+						storage.copy(sourceBucket=source_bucket, sourceObject=source_object, destinationBucket=destination_bucket, destinationObject=my_bam_file, body={}).execute()				
+					except: 
+						# not sure what this will throw yet
+						pass
 
 				# Write the new file path to the file list
 				f.write(new_path)
 			else:
 				f.write(isb_cgc_bam_file)
+	f.close()
 			
 	# create the job config file
-	with open(config_file[0], 'w') as g:
-		g.write("export INPUT_LIST_FILE=%s" % text_file_list[1])
+	with open(config_file.name, 'w') as g:
+		g.write("export INPUT_LIST_FILE=%s" % text_file_list.name)
 		g.write("export OUTPUT_PATH=%s" % output_bucket)
 		g.write("export OUTPUT_LOG_PATH=%s" % logs_bucket)
 		g.write("export SAMTOOLS_OPERATION=\"index\"")
 
-	# submit the job
-	output = subprocess.check_output(["%s/src/samtools/launch_samtools" % grid_computing_tools_dir, config_file[1]])
-	print output
+	g.close()
+
+	if dry_run:
+		print "would've run %s/src/samtools/launch_samtools %s" % (grid_computing_tools_dir, config_file.name)
+	else:
+		# submit the job
+		output = subprocess.check_output(["%s/src/samtools/launch_samtools" % grid_computing_tools_dir, config_file.name])
 
 	if not copy_original_bams:
-		copyfile(text_file_list[0], "%s-isb-cgc-bam-files.txt" % job_name)
+		copyfile(text_file_list.name, "%s-isb-cgc-bam-files.txt" % job_name)
 		
 	
 def generate_file_list_from_sample(sample_barcode, auth_token):
@@ -68,20 +84,20 @@ def generate_file_list_from_sample(sample_barcode, auth_token):
 		"sample_barcode": sample_barcode,
 		"token": auth_token
 	}
+
 	try:
-		response = cgc_curl.get(url, params)
+		response = isb_curl.get(url, params)
 	except:
 		print "There was a problem with the format of the request -- exiting"
-		exit(-1)
+		#exit(-1)
 	else:
 		print response
-		if len(response["datafilenamekeys"]) > 0:
+		if "datafilenamekeys" in response.keys() > 0:
 			for datafilenamekey in response["datafilenamekeys"]:
-				if datafilenamekey is not None and re.search(bam_pattern, datafilenamekey) is not None :
+				if re.search(bam_pattern, datafilenamekey) is not None :
 					file_list.append(datafilenamekey)
 		else:
-			print "No BAM files found for sample %s -- exiting" (sample_barcode)
-			exit(0)
+			print "No BAM files found for sample %s" % (sample_barcode)
 			
 	return file_list
 
@@ -95,18 +111,17 @@ def generate_file_list_from_cohort(cohort_id, auth_token):
 	}
 
 	try:
-		response = cgc_curl.get(url, params)
+		response = isb_curl.get(url, params)
 	except:
 		print "There was a problem with the format of the request -- exiting"
-		exit (-1)
+		#exit (-1)
 	else:
 		print response
 		if "samples" in response.keys() and len(response["samples"]) > 0:
 			for barcode in response["samples"]:
 				file_list.extend(generate_file_list_from_sample(barcode, auth_token))
 		else:
-			print "No BAM files found for cohort %s -- exiting" (cohort_id)
-			exit(0)
+			print "No BAM files found for cohort %s" % cohort_id
 			
 	return file_list
 
@@ -121,16 +136,19 @@ if __name__ == "__main__":
 		help='The destination bucket for all logs.  Must be a valid Google Cloud Storage bucket URL, i.e. gs://bucket_name. Required')
 	parser.add_argument('--grid_computing_tools_dir', required=True,
 		help='Path to the root directory of the "grid-computing-tools" repository. Required')
-	parser.add_argument('--copy_original_bam', required=False, 
-		help='If set to True, will copy the original bam file from the ISB-CGC cloud storage space to the output bucket.  Otherwise a list of the original BAM locations in GCS will be generated in the current working directory.  Default: False')
-	group = parser.add_mutually_exclusive_group(required=True)
-	group.add_argument('--cohort_id', type=int, help='The cohort ID for which you\'d like to index associated BAM files.')
-	group.add_argument('--sample_barcode', type=str, help='The sample barcode for which you\'d like to index associated BAM files.')
+	group1 = parser.add_mutually_exclusive_group(required=True)
+	group1.add_argument('--cohort_id', type=int, help='The cohort ID for which you\'d like to index associated BAM files.')
+	group1.add_argument('--sample_barcode', type=str, help='The sample barcode for which you\'d like to index associated BAM files.')
+	group2 = parser.add_mutually_exclusive_group(required=True)
+	group2.add_argument('--copy_original_bams', action='store_true', default=False,
+		help='If set, will copy the original bam files from the ISB-CGC cloud storage space to the output bucket.  Otherwise a list of the original BAM locations in GCS will be generated in the current working directory.  Default: False')
+	group2.add_argument('--dry_run', action='store_true', default=False,
+		help='If set, will not copy or index any BAM files, but will display the copy and index operations that would have occurred during a real run. Default: False')
 	
 	args = parser.parse_args()
 	
 	# authenticate to ISB-CGC
-	auth_object = cgc_gauth.get_credentials(oauth_flow_args=['--noauth_local_webserver']) # this is hacky... cgc_gauth.py may need some refactoring
+	auth_object = isb_auth.get_credentials()
 
 	# create the cloud storage API object
 	storage_credentials = GoogleCredentials.get_application_default()
@@ -138,12 +156,20 @@ if __name__ == "__main__":
 	
 	# generate a list of files to index
 	if "cohort_id" in args:
+		from_cohort = True
 		file_list = generate_file_list_from_cohort(args.cohort_id, auth_object.access_token)
 		
 	elif "sample_barcode" in args:
+		from_cohort = False
 		file_list = generate_file_list_from_sample(args.sample_barcode, auth_object.access_token)
 	
-	print file_list
-	# run the indexing job
-	index_bam_files(file_list, storage, args.job_name, args.output_bucket, args.log_bucket, args.grid_computing_tools_dir, args.copy_original_bam)
+	if len(file_list) > 0:
+		# run the indexing job
+		index_bam_files(file_list, storage, args.job_name, args.output_bucket, args.logs_bucket, args.grid_computing_tools_dir, args.copy_original_bams, args.dry_run)
+	else:
+		if from_cohort:
+			print "No bam files found for cohort (id: %s)" % str(args.cohort_id)
+		else:
+			user_query = "No bam files found for sample (barcode: %s)" % args.sample_barcode
+
 
