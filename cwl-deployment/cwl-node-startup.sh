f01bb8425d863a821ee9427072670a383ec3322f
smrgit <sheila.reynolds@systemsbiology.org>
2016-01-04 16:13:22 -0800

    added groupadd and chgrp commands

--- cwl-deployment/cwl-node-startup.sh
+++ cwl-deployment/cwl-node-startup.sh
@@ -7,6 +7,9 @@ python get-pip.py && rm get-pip.py
 # Install CWL reference implementation
 pip install cwl-runner
 
+# Add the docker group
+groupadd docker
+
 # Create the data directory used by the example
 mkdir -p /cwl-data/workflows/inputs 
-
+chgrp -R docker /cwl-data
