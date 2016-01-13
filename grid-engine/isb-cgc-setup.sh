e6a44ac6eb2be556456b5fa7b92a46bf66f2ebb8
Abigail Hahn <Abigail.Hahn@systemsbiology.org>
2016-01-13 13:31:53 -0800

    Including elasticluster installation in the setup script for the grid engine examples

--- grid-engine/isb-cgc-setup.sh
+++ grid-engine/isb-cgc-setup.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
+cd $1 # install dir given as first positional param
+# install dependencies
 sudo apt-get -qq update
 sudo apt-get -qq install git python-dev libffi-dev
 wget https://bootstrap.pypa.io/get-pip.py && sudo python get-pip.py && rm get-pip.py
-sudo pip install pyopenssl ndg-httpsclient pyasn1 requests
-sudo pip install --upgrade google-api-python-client
+sudo pip install virtualenv
+sudo gcloud -q components update
+# set up a virtualenv for the examples
+mkdir virtualenv
+cd virtualenv
+virtualenv isb_compute_examples
+source isb_compute_examples/bin/activate
+pip install pyopenssl ndg-httpsclient pyasn1 requests 
+pip install --upgrade google-api-python-client
+# install elasticluster
+cd isb_compute_examples
+git clone https://github.com/gc3-uzh-ch/elasticluster.git 
+cd elasticluster && python setup.py install && cd ..
+# install isb code repos
 git clone https://github.com/isb-cgc/examples-Compute.git
 git clone https://github.com/isb-cgc/ISB-CGC-Webapp.git
 git clone https://github.com/googlegenomics/grid-computing-tools.git
-sudo gcloud -q components update
+# set up elasticluster configuration
+mkdir ~/.elasticluster && touch ~/.elasticluster/config
+cp -R examples-Compute/grid-engine/elasticluster/config.d ~/.elasticluster
 
 export ISB_ACCESS_SCRIPTS_DIR=$PWD/ISB-CGC-Webapp/scripts
 export GRID_COMPUTING_TOOLS_DIR=$PWD/grid-computing-tools
