93f9e071137d3f50fd7c62ce8b5e62e2f7fca64c
Abigail Hahn <vardaofthevalier@users.noreply.github.com>
2016-02-17 13:36:34 -0800

    Rearranged the order of commands

--- kubernetes/workstation_setup.sh
+++ kubernetes/workstation_setup.sh
@@ -4,12 +4,12 @@ sudo apt-get -qq update
 sudo apt-get -qq install python-dev python-software-properties libffi-dev git
 wget https://bootstrap.pypa.io/get-pip.py && sudo python get-pip.py && rm get-pip.py
 sudo pip install virtualenv
+sudo gcloud components install kubectl
+sudo gcloud components install beta
+sudo gcloud components update
 if [[ ! -d $HOME/virtualenv ]]; then mkdir -p $HOME/virtualenv; fi
 virtualenv $HOME/virtualenv/toil
 source $HOME/virtualenv/toil/bin/activate
 pip install toil requests json-spec httplib2 google-api-python-client
-sudo gcloud components install kubectl
-sudo gcloud components install beta
-sudo gcloud components update
 
 
