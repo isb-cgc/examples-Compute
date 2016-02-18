19aa41aa23ab954238a674194e84948f3b297445
Abigail Hahn <ahahn@systemsbiology.org>
2016-02-18 13:47:09 -0800

    Updated the workstation setup script

--- kubernetes/workstation_setup.sh
+++ kubernetes/workstation_setup.sh
@@ -4,9 +4,9 @@ sudo apt-get -qq update
 sudo apt-get -qq install python-dev python-software-properties libffi-dev git
 wget https://bootstrap.pypa.io/get-pip.py && sudo python get-pip.py && rm get-pip.py
 sudo pip install virtualenv
-sudo gcloud components install kubectl
-#sudo gcloud components install beta
 sudo gcloud components update
+sudo gcloud components install kubectl
+echo "export PATH=$PATH:/usr/local/share/google/google-cloud-sdk/bin" >> ~/.bashrc
 if [[ ! -d $HOME/virtualenv ]]; then mkdir -p $HOME/virtualenv; fi
 virtualenv $HOME/virtualenv/toil
 source $HOME/virtualenv/toil/bin/activate
