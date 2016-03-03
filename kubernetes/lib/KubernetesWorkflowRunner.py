696a0704014bd097c4215835cade0191bb38b973
Abigail Hahn <Abigail.Hahn@systemsbiology.org>
2016-03-03 14:40:15 -0800

    Fixed indentation error

--- kubernetes/lib/KubernetesWorkflowRunner.py
+++ kubernetes/lib/KubernetesWorkflowRunner.py
@@ -124,6 +124,8 @@ class KubernetesWorkflowRunner():
 				print "Job {job_name} must either add an additional disk or use a host key to select a particular host to be scheduled".format(job_name=job["name"])
 				exit(-1) 
 			elif not "host_key" in job.keys() and not "add_disk" in job["resources"].keys():
+				print "Job {job_name} must either add an additional disk or use a host key to select a particular host to be scheduled".format(job_name=job["name"])
+				exit(-1)
 			if "parents" in job.keys():
 				for parent in job["parents"]:
 					if parent not in job_names:
