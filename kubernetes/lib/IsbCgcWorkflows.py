53a14fe1fbb4c2860a1e783e305774ef8d660e0a
Abigail Hahn <Abigail.Hahn@systemsbiology.org>
2016-03-16 11:37:42 -0700

    Fixed a SyntaxError

--- kubernetes/lib/IsbCgcWorkflows.py
+++ kubernetes/lib/IsbCgcWorkflows.py
@@ -253,7 +253,7 @@ class QcWorkflow(Workflow):
 				data_staging_step = self.createDefaultDataStagingStep(filename, analysis_id, url)
 				qc_step = self.createDefaultQcStep(filename, analysis_id)
 				cleanup_step = self.createDefaultCleanupStep(filename, analysis_id, self.output_bucket)
-				cleanup_bai = self.createDefaultCleanupStep("{filename}.bai".format(filename=filename), analysis_id, '/'.join(url.split('/')[0:-1])
+				cleanup_bai = self.createDefaultCleanupStep("{filename}.bai".format(filename=filename), analysis_id, '/'.join(url.split('/')[0:-1]))
 				self.__create_subworkflow([ data_staging_step, qc_step, cleanup_step, cleanup_bai ], subworkflow_name, host_key)
 				host_key += 1
 	
